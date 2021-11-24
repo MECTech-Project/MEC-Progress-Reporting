fecbf31f3436abd4baa25357c047f57b889685c1
George <george@hotmail.com>
2021-11-24 21:35:42 +0200

    Time option added

--- src/Components/ReportPage.js
+++ src/Components/ReportPage.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import{Card, Nav, Button,Container,Col} from 'react-bootstrap';
-
+import "../App.css"
+import WorkTime from "./WorkTime"
 function ReportPage() {
     return (
-        <div>
+        <div className="plancards">
         <Container>
         <Col  xs={6} md={6} lg={6} >
+        <Card.Title>Did the plan go as planned ?</Card.Title>
+        <br></br>
+
+        {/*First Card*/}
         <Card>
   <Card.Header>
-  
     <Nav variant="pills" defaultActiveKey="#first">
+    <Card.Title>Plan 1</Card.Title>
     <div className='buttons'>
     <Button  variant="primary">Yes</Button>
     <Button className ="no" variant="primary">No</Button>
@@ -20,15 +25,41 @@ function ReportPage() {
     <Card.Title>Please fill in the report for the plan</Card.Title>
     <Card.Text>
       With supporting text below as a natural lead-in to additional content.
+      <WorkTime />
+     
     </Card.Text>
     
   </Card.Body>
 </Card>
 <br></br>
 
+{/*Second Card*/}
 <Card>
   <Card.Header>
     <Nav variant="pills" defaultActiveKey="#first">
+    <Card.Title>Plan 2</Card.Title>
+    <div className='buttons'>
+    <Button variant="primary">Yes</Button>
+    <Button className ="no" variant="primary">No</Button>
+    </div>
+    </Nav>
+  </Card.Header>
+  <Card.Body>
+    <Card.Title>Please fill in the report for the plans</Card.Title>
+    <Card.Text>
+      With supporting text below as a natural lead-in to additional content.
+      <WorkTime />
+    </Card.Text>
+    
+  </Card.Body>
+</Card>
+<br></br>
+
+{/*Third Card*/}
+<Card>
+  <Card.Header>
+    <Nav variant="pills" defaultActiveKey="#first">
+    <Card.Title>Plan 3</Card.Title>
     <div className='buttons'>
     <Button variant="primary">Yes</Button>
     <Button className ="no" variant="primary">No</Button>
@@ -39,34 +70,18 @@ function ReportPage() {
     <Card.Title>Please fill in the report for the plan</Card.Title>
     <Card.Text>
       With supporting text below as a natural lead-in to additional content.
-    </Card.Text>
-    
-  </Card.Body>
-</Card>
-<br></br>
-
-<Card>
-  <Card.Header>
-    <Nav variant="pills" defaultActiveKey="#first">
-    <div className='buttons'>
-    <Button variant="primary">Yes</Button>
-    <Button className ="no" variant="primary">No</Button>
-    </div>
-    </Nav>
-  </Card.Header>
-  <Card.Body>
-    <Card.Title>Please fill in the report for the plan</Card.Title>
-    <Card.Text>
-      With supporting text below as a natural lead-in to additional content.
+      <WorkTime />
     </Card.Text>
   
   </Card.Body>
 </Card>
 <br></br>
 
+{/*Fourth Card*/}
 <Card>
   <Card.Header>
     <Nav variant="pills" defaultActiveKey="#first">
+    <Card.Title>Plan 4</Card.Title>
     <div className='buttons'>
     <Button variant="primary">Yes</Button>
     <Button className ="no" variant="primary">No</Button>
@@ -77,6 +92,7 @@ function ReportPage() {
     <Card.Title>Please fill in the report for the plan</Card.Title>
     <Card.Text>
       With supporting text below as a natural lead-in to additional content.
+      <WorkTime />
     </Card.Text>
  
   </Card.Body>
