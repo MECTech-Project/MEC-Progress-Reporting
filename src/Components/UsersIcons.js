a84c8943588f3fced2702c6d78fa0fcf6efb5338
caroltawaify <carol.tawaify96@gmail.com>
2021-11-29 20:34:52 -0500

    Add a file

--- src/Components/UsersIcons.js
+++ src/Components/UsersIcons.js
@@ -1,37 +1,41 @@
-import React from "react";
-import { Card, Button ,Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Card, Button, Col } from "react-bootstrap";
 import UsersList from "./users";
 
-function handleClick(){
-  console.log("I was clicked")
-}
 
+function UsersIcons() {
 
-function UsersIcons(){
-    let users = UsersList.map((user) => {
+  const [deleted, setDeleted] = useState(UsersList);
 
-        return (
-            <Col xs={6} md={4} lg={4} key={user.id}>
-              <Card  border="dark" style={{ backgroundColor: '#c097c5'}}>
+  function handleClick() {
+    console.log("I was clicked")
+  }
+
+  function handleDeleteUser(id) {
+    setDeleted(deleted.filter(user => user.id !== id))
+    console.log(" deleted")
+  }
+  return (
+    <div className="container">
+      <h2 className="text-center">Students Page</h2>
+      <div className="group row">
+        {deleted.map(user => (
+          <Col xs={6} md={4} lg={4} key={user.id}>
+            <Card border="dark" style={{ backgroundColor: '#c097c5' }}>
               <Card.Body className="text-center">
-               <Card.Title>{user.name}</Card.Title>
-               <hr/>
-               <Button variant="dark" onClick={handleClick}>View User</Button>
-               <hr/>
-               <Button variant="dark" onClick={handleClick}>Delete User</Button>
-               </Card.Body>
-              </Card> 
-            </Col>
-          );
-    });
-    return (
-        <div className="container">
-          <h2 className="text-center">Students Page</h2>
-          <div className="group row">
-            {users}    
-          </div>
-        </div>
-      )
+                <Card.Title>{user.name}</Card.Title>
+                <hr />
+                <Button variant="dark" onClick={handleClick}>View User</Button>
+                <hr />
+                <Button variant="dark" onClick={() => handleDeleteUser(user.id)}>Delete User</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+        );
+      </div>
+    </div>
+  )
 }
 
 export default UsersIcons
