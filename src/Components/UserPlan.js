ed98b1f9a1018f769d2c59e415c0358fb1d9ccbc
caroltawaify <carol.tawaify96@gmail.com>
2021-11-30 16:37:55 -0500

    Add a file

--- src/Components/UserPlan.js
+++ src/Components/UserPlan.js
@@ -1,48 +1,102 @@
-import React, { useState } from "react";
-import { Stack, Form, Button } from "react-bootstrap";
+import React, { useState, useRef, useEffect } from "react";
+import { Stack, Button } from "react-bootstrap";
+import WorkTime from "./WorkTime";
 
 const initialList = [
     {
-        plan: 1
-    },
-    {
-        plan: 2
-    },
-    {
-        plan: 3
+        plan: 1,
     }
 
 ];
 
 function UserPlan() {
 
-    const [lists, setList] = useState(initialList)
+    const [lists, setList] = useState(initialList);
+
+    const [planText, setPlanText] = useState("")
+
+    const inputRef = useRef([])
+    // inputRef.current = []
+    function submit(e) {
+        console.log(inputRef.current.value)
+        // if (e && !inputRef.current.includes(e)) {
+        //     inputRef.current.push(e);
+        // }
+
+    }
 
     function handleAddPlan() {
         setList([...lists, {
-            plan: lists.length += 1
+            plan: lists.length + +1
         }])
-    }
-    function handleRemove(plan){
+    };
+    function handleRemove(plan) {
         setList(lists.filter(list => list.plan !== plan))
     };
+    const handlePlanChange = (e) => {
+        setPlanText(e.target.value);
+    }
+    // const addtolist = (e) =>{
+    //     if(e && inputRef.current.includes(e)){
+    //         inputRef.current.push(e);
+    //     }
+    //     console.log(inputRef.current)
+    // }
+
+
+    const Component = () => {
+
+        const [length, setLength] = useState(2);
+        const refs = useRef([React.createRef(), React.createRef()]);
+      
+        function updateLength({ target: { value }}) {
+          setLength(value);
+          refs.current = refs.current.splice(0, value);
+          for(let i = 0; i< value; i++) {
+            refs.current[i] = refs.current[i] || React.createRef();
+          }
+          refs.current = refs.current.map((item) => item || React.createRef());
+        }
+      
+        useEffect(() => {
+         refs.current[refs.current.length - 1].current.focus()
+        }, [length]);
+      
+        return (<>
+          <ul>
+          {refs.current.map((el, i) =>
+            <li key={i}><input ref={refs.current[i]} value={i} /></li>
+          )}
+        </ul>
+        <input value={refs.current.length} type="number" onChange={updateLength} />
+        </>)
+      }
+
+
     return (
         <div className="container">
             <h1 className="text-center">Fill your Plan</h1>
-            <ul>
-                {lists.map(list => (
-                    <li key={list.plan}>
-                        <Stack direction="horizontal" gap={5}>
-                            <Form.Control className="me-auto" placeholder="Add your plan here..." />
-                            <input type="time" min="09:00" max="18:00" required />
-                            <div className="vr" />
-                            <Button  variant="outline-light" onClick={() => handleRemove(list.plan)}>✖</Button>
-                        </Stack>
-                    </li>
-                ))}
+            <ul >
+                {Component}
+                {/* {
+                    lists.map(list => (
+                        <li value={planText} onChange={handlePlanChange} key={list.plan}>
+                            <Stack direction="horizontal" gap={3}>
+                                <textarea ref={inputRef} type="text" className="me-auto" placeholder="Add your plan here..." />
+                                <div style={{ width: "140px" }}>
+                                    <WorkTime />
+                                </div>
+                                <div className="vr" />
+                                <Button variant="outline-light" onClick={() => handleRemove(list.plan)}>✖</Button>
+                            </Stack>
+                        </li>
+                    ))
+                } */}
+                {/* {renderPlan ? console.log("Submitted") : null } */}
             </ul>
             <Button variant="outline-light" onClick={handleAddPlan}> Add a Plan </Button>
-
+            <div className="vr" />
+            <Button onClick={submit} type="submit" variant="outline-light" > Submit </Button>
         </div>
     )
 
