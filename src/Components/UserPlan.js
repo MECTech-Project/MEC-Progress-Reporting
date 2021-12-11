7c04c140da3f7d552e4692caefa9114731c151a9
caroltawaify <carol.tawaify96@gmail.com>
2021-12-11 00:30:26 -0500

    New

--- src/Components/UserPlan.js
+++ src/Components/UserPlan.js
@@ -1,105 +1,117 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Stack, Button } from "react-bootstrap";
-import WorkTime from "./WorkTime";
+import { TimePickerComponent } from '@syncfusion/ej2-react-calendars';
 
 
 const initialList = [
     {
-        plan: 1,
+        id: 1,
         name: "task 1",
-        remove: true
+        remove: true,
+        time: 0
     },
     {
-        plan: 2,
+        id: 2,
         name: "task 2",
-        remove: true
+        remove: true,
+        time: 0
 
     },
     {
-        plan: 3,
+        id: 3,
         name: "task 3",
-        remove: true
+        remove: true,
+        time: 0
+
     }
+
 ];
 
-export default function App() {
+export default function UserPlan() {
     const { register, handleSubmit, resetField } = useForm();
     const [lists, setList] = useState(initialList);
 
-    const onSubmit = (data) => console.log(data);
+    const onSubmit = (data,event) => {
+        console.log(data, event);
+    }
 
     function handleAddPlan() {
         setList([
             ...lists,
             {
-                plan: lists.length + 1,
+                id: lists.length + 1,
                 name: `task ${lists.length + 1}`,
-                remove: false
+                remove: false,
+                // time: 0
             }
         ]);
         console.log("lists", lists)
     }
 
-    function handleRemove(plan, remove, name) {              
+    function handleRemove(id, remove, name) {
         if (remove === false) {
 
             let ifTrue = lists.filter((list) => list.remove === true);
             console.log("ifTrue", ifTrue)
             let ifFalse = lists.filter((list) => list.remove === false);
             console.log("ifFalse", ifFalse)
-            let main = ifFalse.filter((list) => list.plan !== plan);
+            let main = ifFalse.filter((list) => list.id !== id);
 
             resetField(`${name}`)
             console.log("name", name)
-            
+
             let falseRemove = main.map((x, i) => {
                 return {
-                    plan: i + 4,
+                    id: i + 4,
                     name: `task ${i + 4}`,
-                    remove: false
+                    remove: false,
+                    // time: lists.time
                 }
             })
-            
+
             setList([
                 ...ifTrue, ...falseRemove
             ]);
             console.log("falseRemove", falseRemove)
-            
         }
-        
+    }
+
+    function handleChange(event) {
+        console.log("e", event)
+    //     // let timing = lists.push({time:event})
+    //     setList(lists => [...lists, {time:event}])
     }
 
     const inputs = lists.map((list) => {
 
         return (
 
-            <div key={list.plan}>
+            <div key={list.id}>
                 <Stack direction="horizontal" gap={3}>
                     <textarea
                         className="me-auto"
                         {...register(`${list.name}`, { required: true })}
-                        placeholder={`plan ${list.plan} ${list.name}`}
+                        placeholder={`Plan ${list.id} ${list.name}`}
                     />
-                    <div style={{ width: "80px" }}>
-                        <WorkTime />
-                    </div>
+                    <TimePickerComponent 
+                    {...register(`${list.time}`, { required: true })}
+                    placeholder={`time ${list.id}`}
+                    format='HH:mm'
+                    onChange={(event)=> handleChange(event)}
+                    >
+                    </TimePickerComponent>
                     <div className="vr" />
-                    <Button onClick={() => handleRemove(list.plan, list.remove, list.name)} >✖</Button>
+                    <Button onClick={() => handleRemove(list.id, list.remove, list.name)} >✖</Button>
                 </Stack>
 
             </div>
         );
     });
 
-    // useEffect(() => {
-    //     console.log("render")
-    //     lists.addEventListener('', handleRemove)
-    // },[])
-
     return (
         <div className="container">
-            <h1 className="text-center">Fill your Plan</h1>
+            <h1 className="text-center">Fill your plan</h1>
             {inputs}
             <Button type="submit" onClick={handleSubmit(onSubmit)}>Submit</Button>
             <Button onClick={handleAddPlan}>Add</Button>
