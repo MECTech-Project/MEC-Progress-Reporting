d577fcd39aeacbde9832592d88e729b3a559f360
Majd-Mora <majedmoura99@gmail.com>
2021-12-29 00:01:29 +0200

    put the answer and understanding it

--- src/Components/DateRangePicker.js
+++ src/Components/DateRangePicker.js
@@ -1,124 +1,101 @@
-import React, { useState, useEffect } from 'react';
-import { DateRangePickerComponent } from '@syncfusion/ej2-react-calendars';
-import '../Styles/DateRangePicker.css';
-// import DatePicker from 'react-datepicker';
-// import "react-datepicker/dist/react-datepicker.css";
-import { ListGroup, Card } from 'react-bootstrap';
-import plansData from './Plans';
+import React, { useState, useEffect } from "react";
+import "../Styles/DateRangePicker.css";
+import DateRangePicker from "react-bootstrap-daterangepicker";
 
-// showing before 5 days ago and the format on the date range picker.
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-const ddd = dd - 5;
-const BfiveDays = today = ddd + '/' + mm + '/' + yyyy;
-today = dd + '/' + mm + '/' + yyyy;
-const initDate = `${BfiveDays} - ${today}`
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-daterangepicker/daterangepicker.css";
 
-
-
-
-
-
-
-// const input = startDate && endDate
-// const inputTs = Date.parse(input);
-// const result = plansData.filter(d => Date.parse(d.date) <= inputTs);
-
-// console.log(result)
-
+import { ListGroup, Card } from "react-bootstrap";
+import plansData from "./Plans";
 
 function WorkDate() {
-    const [dates, setDates] = useState({ value: initDate });
-    const [startDate, setStartDate] = useState('');
-    const [endDate, setEndDate] = useState('');
-    const [result, setResult] = useState([]);
+  const [result, setResult] = useState([]);
+  const [BfiveDays, setBfiveDays] = useState([]);
+  const [today, setToday] = useState([]);
+  const [startDate, setStartDate] = useState([]);
+  const [endDate, setEndDate] = useState([]);
 
+  useEffect(() => {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, "0");
+    var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+    var yyyy = today.getFullYear();
+    const ddd = dd - 5;
+    const BfiveDays = (today = mm + "/" + ddd + "/" + yyyy);
+    today = mm + "/" + dd + "/" + yyyy;
+    setToday(today);
+    setBfiveDays(BfiveDays);
+    const results = plansData.filter(function (obj) {
+      return obj.date >= BfiveDays && obj.date <= today;
+    });
+    setResult(results);
+  }, []);
 
+  const handleCallback = (start, end, label) => {
+    let startDate = formatDate(start);
+    let endDate = formatDate(end);
+    setStartDate(startDate);
+    setEndDate(endDate);
 
+    console.log("start", startDate, endDate);
+    const results = plansData.filter(function (obj) {
+      return obj.date >= startDate && obj.date <= endDate;
+    });
+    setResult(results);
+    setToday(false);
+    console.log("results", results);
+  };
 
-    const onChange = date => {
-        // setDate(date);
-        setDates(date);
-        // let startD = date.target.closeEventArgs.date.substring(0,10) 
-        // console.log(startD)
-
-        console.log("date", dates);
-        let startD = date.target.closeEventArgs.date.substring(0, 10)
-        let endD = date.target.closeEventArgs.date.substring(12, 23)
-        setStartDate(startD)
-        setEndDate(endD)
-        console.log("dateeee", endD);
-    filter()
-
-    };
-
-
-    const filter = () => {
-       
-        const results = plansData.filter(function (obj) {
-            return obj.date >= startDate && obj.date <= endDate;
-        });
-        setResult(results)
-        console.log(results)
-    }
-
-    // useEffect(() => {
-
-
-    //     return () => {
-    //     }
-    // }, [startDate, endDate, dates])
-
-    // Mapping plansData (Cards)
-    let plans = result.map((plan) => {
-        return (
-            <div key={plan.id}>
-                <Card className="text-center" key={plan.id}>
-                    <ListGroup.Item variant="primary">Date of:  </ListGroup.Item>
-                    <ListGroup.Item variant="dark"><h4>{plan.task}</h4><center>{plan.plan}</center>
-                    </ListGroup.Item>
-                    <Card.Body>
-                        <Card.Text>
-                            {plan.report}
-                        </Card.Text>
-                    </Card.Body>
-                    <ListGroup.Item variant="dark"><center>{plan.plan}</center></ListGroup.Item>
-                    <Card.Body>
-                        <Card.Text>
-                            {plan.report}
-                        </Card.Text>
-                    </Card.Body>
-                    <ListGroup.Item variant="dark"><center>{plan.plan}</center></ListGroup.Item>
-                    <Card.Body>
-                        <Card.Text>
-                            {plan.report}.
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            </div>
-        )
-    })
-
-
+  const formatDate = (start) => {
+    var dd = String(start._d.getDate()).padStart(2, "0");
+    var mm = String(start._d.getMonth() + 1).padStart(2, "0"); //January is 0!
+    var yyyy = start._d.getFullYear();
+    let today = mm + "/" + dd + "/" + yyyy;
+    return today;
+  };
+  let plan = result.map((plan) => {
     return (
-        <div>
-            {/* date range picker component */}
-            <DateRangePickerComponent placeholder="Enter Date Range"
-                onChange={onChange}
-                value={dates.value}
-                dateFormat="yyyy-MM-dd"
-            /><br />
+      <div key={plan.id}>
+        <Card className="text-center" key={plan.id}>
+          <ListGroup.Item variant="primary">Date of: </ListGroup.Item>
+          <ListGroup.Item variant="dark">
+            <h4>{plan.task}</h4>
+            <center>{plan.plan}</center>
+          </ListGroup.Item>
+          <Card.Body>
+            <Card.Text>{plan.report}</Card.Text>
+          </Card.Body>
+          <ListGroup.Item variant="dark">
+            <center>{plan.plan}</center>
+          </ListGroup.Item>
+          <Card.Body>
+            <Card.Text>{plan.report}</Card.Text>
+          </Card.Body>
+          <ListGroup.Item variant="dark">
+            <center>{plan.plan}</center>
+          </ListGroup.Item>
+          <Card.Body>
+            <Card.Text>{plan.report}.</Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+    );
+  });
 
-<p>{endDate}</p>
-
-            <br />{plans}<br />
-        </div>
-
-    )
+  return (
+    <div>
+      <DateRangePicker
+        // locale={{ format: "DD/MM/YYYY" }}
+        onCallback={handleCallback}
+        initialSettings={{ startDate: BfiveDays, endDate: today }}
+      >
+        <button>
+          {today ? `${BfiveDays} - ${today}` : `${startDate} - ${endDate}`}
+        </button>
+      </DateRangePicker>
+      {plan}
+    </div>
+  );
 }
-
-
 
 export default WorkDate;
--- src/Components/Plans.js
+++ src/Components/Plans.js
@@ -1,39 +1,40 @@
 const plansData = [
     {
-        id: 1,
-        task: "one",
-        plan: "I will study js course 1",
-        report: "I worked on js 1",
-        date: "12/20/2021"
+      id: 1,
+      task: "one",
+      plan: "I will study js course 1",
+      report: "I worked on js 1",
+      date: "12/23/2021"
     },
     {
-        id: 2,
-        task: "two",
-        plan: "I will study js course 2",
-        report: "I worked on js 2",
-        date: "12/20/2021"
+      id: 2,
+      task: "two",
+      plan: "I will study js course 2",
+      report: "I worked on js 2",
+      date: "12/19/2021"
     },
     {
-        id: 3,
-        task: "three",
-        plan: "I will study js course 3",
-        report: "I worked on js 3",
-        date: "12/10/2021"
+      id: 3,
+      task: "three",
+      plan: "I will study js course 3",
+      report: "I worked on js 3",
+      date: "12/02/2021"
     },
     {
-        id: 4,
-        task: "four",
-        plan: "I will study js course 4",
-        report: "I worked on js 4",
-        date: "12/20/2021"
+      id: 4,
+      task: "four",
+      plan: "I will study js course 4",
+      report: "I worked on js 4",
+      date: "10/23/2021"
     },
     {
-        id: 5,
-        task: "five",
-        plan: "I will study js course 5",
-        report: "I worked on js 5",
-        date: "12/20/2021"
+      id: 5,
+      task: "five",
+      plan: "I will study js course 5",
+      report: "I worked on js 5",
+      date: "11/23/2021"
     }
-];
-
-export default plansData;
+  ];
+  
+  export default plansData;
+  
