41270f327c7ffff27aa17abcb74f9b3d99fabc43
caroltawaify <carol.tawaify96@gmail.com>
2021-12-28 09:31:59 -0500

    New UserPlan

--- src/Components/UserPlan.js
+++ src/Components/UserPlan.js
@@ -1,145 +1,89 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import { Stack, Button } from "react-bootstrap";
+import React from "react";
+import { useForm, useFieldArray } from "react-hook-form";
+// import { whatever } from "react-bootstrap";
 
 
-const initialList = [
-    {
-        id: 1,
-        name: "task 1",
-        remove: true,
-        time: "time 1"
-    },
-    {
-        id: 2,
-        name: "task 2",
-        remove: true,
-        time: "time 2"
-
-    },
-    {
-        id: 3,
-        name: "task 3",
-        remove: true,
-        time: "time 3"
-
+function UserPlan() {
+  const { register, control, handleSubmit } = useForm({
+    defaultValues: {
+      plan: [
+          { task: "", time: "" },
+          { task: "", time: "" },
+          { task: "", time: "" }
+        ]
     }
+  });
+  const {
+    fields,
+    append,
+    remove,
+  } = useFieldArray({
+    control,
+    name: "plan"
+  });
 
-];
+  const onSubmit = (data) => console.log(data);
 
-export default function UserPlan() {
-    const { register, handleSubmit, resetField } = useForm();
-    const [lists, setList] = useState(initialList);
+  return (
+    <center>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h1> Fill your plan </h1>
+       <ol>
+        {fields.map((item, index) => {
+          return (
 
-    const onSubmit = (data) => {
-    //     let cleanData = [];
-    //     for(const [key, value] of Object.entries(data)) {
-    //         const number = parseInt(key.charAt(key.length-1))-1
-    //         if (!cleanData[number]) {
-    //             cleanData.push([value]);
-    //         } else {
-    //             cleanData[number].push(value); 
-    //         }
-    //     }
-    //     console.log(`data`,cleanData);
-    for (let i = 0; i < 2; i++) {
-        const element = "me";
-        for(const [key, value] of Object.entries(data))
-        console.log("from for", {[key]: value})
-        
-    }
-    }
+            <li key={item.id}>
+              <textarea placeholder="task"
+                {...register(`plan.${index}.task`, { required: true })} />
 
-    function handleAddPlan() {
-        setList([
-            ...lists,
-            {
-                id: lists.length + 1,
-                name: `task ${lists.length + 1}`,
-                remove: false,
-                time: `time ${lists.length + 1}`
-            }
-        ]);
-    }
+                <select {...register(`plan.${index}.time`, {
+                        required: true,
+                        validate: value =>
+                            value !== "00:00"
+                    })} >
+                    <option />
+                    <option value="00:15">00:15</option>
+                    <option value="00:30">00:30</option>
+                    <option value="01:00">01:00</option>
+                    <option value="01:30">01:30</option>
+                    <option value="02:00">02:00</option>
+                    <option value="02:30">02:30</option>
+                    <option value="03:00">03:00</option>
+                    <option value="03:30">03:30</option>
+                    <option value="04:00">04:00</option>
+                    <option value="04:30">04:30</option>
+                    <option value="05:00">05:00</option>
+                    <option value="05:30">05:30</option>
+                    <option value="06:00">06:00</option>
+                    <option value="06:30">06:30</option>
+                    <option value="07:00">07:00</option>
+                    <option value="07:30">07:30</option>
+                    <option value="08:00">08:00</option>
+                </select >
 
-    function handleRemove(id, remove, name, time) {
-        if (remove === false) {
+              <button type="button" onClick={() => index > 2 ? remove(index) : null}>
+                Delete
+            </button>
+            </li>
 
-            let ifTrue = lists.filter((list) => list.remove === true);
-            console.log("ifTrue", ifTrue)
-            let ifFalse = lists.filter((list) => list.remove === false);
-            console.log("ifFalse", ifFalse)
-            let main = ifFalse.filter((list) => list.id !== id);
+          );
+        })}
+      </ol>
+      <section>
+        <button
+          type="button"
+          onClick={() => {
+            append({ task: "", time: "" });
+          }}
+        >
+          add
+      </button>
 
-            resetField(`${name} ${time}`)
-            console.log("name", name, time)
+      </section>
 
-            let falseRemove = main.map((e, i) => {
-                return {
-                    id: i + 4,
-                    name: `task ${i + 4}`,
-                    remove: false,
-                    time: e.time
-                }
-            })
-
-            setList([
-                ...ifTrue, ...falseRemove
-            ]);
-            console.log("falseRemove", falseRemove)
-        }
-    }
-
-    const inputs = lists.map((list) => {
-        return (
-
-            <div key={list.id}>
-                <Stack direction="horizontal" >
-                    <textarea
-                        type="text"
-                        className="me-auto"
-                        {...register(`${list.name}`, { required: true })}
-                        placeholder={`Plan ${list.id} ${list.name}`}
-                    />
-                    <div className="dropdown">
-                        <div >
-                        <select className="dropdown-content dropbtn" {...register(`${list.time}`, { required: true })} >
-                            <option />
-                            <option value="00:15">00:15</option>
-                            <option value="00:30">00:30</option>
-                            <option value="01:00">01:00</option>
-                            <option value="01:30">01:30</option>
-                            <option value="02:00">02:00</option>
-                            <option value="02:30">02:30</option>
-                            <option value="03:00">03:00</option>
-                            <option value="03:30">03:30</option>
-                            <option value="04:00">04:00</option>
-                            <option value="04:30">04:30</option>
-                            <option value="05:00">05:00</option>
-                            <option value="05:30">05:30</option>
-                            <option value="06:00">06:00</option>
-                            <option value="06:30">06:30</option>
-                            <option value="07:00">07:00</option>
-                            <option value="07:30">07:30</option>
-                            <option value="08:00">08:00</option>
-                        </select >
-                        </div>
-                    </div>
-
-                    <div className="vr" />
-                    <Button onClick={() => handleRemove(list.id, list.remove, list.name)} >✖</Button>
-                </Stack>
-
-            </div>
-        );
-    });
-
-    return (
-        <div className="container">
-            <h1 className="text-center">Fill your plan</h1>
-            {inputs}
-            <Button type="submit" onClick={handleSubmit(onSubmit)}>Submit</Button>
-            <Button onClick={handleAddPlan}>Add</Button>
-        </div>
-    );
+      <button type="submit" > Submit </button>
+    </form>
+    </center>
+  );
 }
+export default UserPlan;
