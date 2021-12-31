8904662b8fc695d99ea867f40d105be8a046dbea
Majd-Mora <majedmoura99@gmail.com>
2021-12-31 23:44:04 +0200

    adding handle click to radio button

--- src/Components/RadioButtons.js
+++ src/Components/RadioButtons.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 function RadioButtons() {
-    const [option, setOption] = useState("");
+    const [option, setOption] = useState();
+    // const [value, setValue] = useState("")
 
     function handleChange(e) {
         setOption(e.target.value);
     }
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log({option})
+    }
+    
     return (
         <div className="App">
             <center>
@@ -15,7 +22,9 @@ function RadioButtons() {
             <input type="radio" name="option" value="no" onChange={handleChange}/>No
             <h1>{option}</h1>
 
-            <Button>see effect</Button>
+            <Form onSubmit={handleSubmit}>
+            <Button onClick={handleSubmit} >see effect</Button>
+            </Form>
             </center>
         </div>
     )
