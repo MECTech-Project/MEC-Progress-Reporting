38a53d8ada24f63d02ce70b613077bd2c465dad2
Majd-Mora <majedmoura99@gmail.com>
2022-01-13 01:20:14 +0200

    worked on index and Users

--- server/index.js
+++ server/index.js
@@ -1,10 +1,34 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const UserModel = require('./models/Users');
+
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
 
 mongoose.connect(
     "mongodb+srv://electro-99:electro.99@cluster0.txzcl.mongodb.net/mec-reports?retryWrites=true&w=majority"
     );
+
+app.get("/getUsers", (req, res) => {
+    UserModel.find({}, (err, result) => {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+app.post("/createUser", async (req, res) => {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+    
+    res.json(user)
+});
 
 app.listen(5000, () => {
     console.log("Server is running on port (5000)");
