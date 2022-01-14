dec148897cc92414443e6a671a4ac12f778a57d4
Majd-Mora <majedmoura99@gmail.com>
2022-01-14 23:07:35 +0200

    added access to email id and password

--- client/src/Components/AddUser.js
+++ client/src/Components/AddUser.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Form, Button, Row, Col, Card, Container} from 'react-bootstrap';
+import Axios from 'axios';
 
 
 function AddUser() {
-    return (
+    // const [listOfUsers, setListOfUsers] = useState([]);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    // useEffect(() => {
+    //     // Axios.get("http://localhost:5000/getUsers").then((response) => {
+    //     //     setListOfUsers(response.data);
+    //     // });
+    // }, []);
+
+
+    const createUser = () => {
+        Axios.post("http://localhost:5000/createUser", {
+            name,
+            email,
+            password,
+        }).then((response) => {
+            alert("USER HAS BEEN CREATED");
+        });
+    };
     
+
+
+    return (
     <div>
     <Container>
   <Row className="justify-content-md-center">
@@ -14,26 +38,32 @@ function AddUser() {
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Full Name</Form.Label>
-        <Form.Control type="username" placeholder="Enter Name" />
+        <Form.Control type="username" placeholder="Enter Name" onChange={(e) => {
+            setName(e.target.value);
+        }} />
         <Form.Text className="text-muted">
         </Form.Text>
     </Form.Group>
 
     <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" onChange={(e) => {
+            setEmail(e.target.value);
+        }}/>
         <Form.Text className="text-muted">
         </Form.Text>
     </Form.Group>
 
     <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder="Password" onChange={(e) => {
+            setPassword(e.target.value);
+        }}/>
     </Form.Group>
     <Form.Group className="mb-3" controlId="formBasicCheckbox">
         
     </Form.Group>
-    <Button variant="primary" type="submit" onClick={()=> console.log("Submitted")}>
+    <Button variant="primary" onClick={createUser}>
         Submit
     </Button>
     </Form>
--- server/models/Users.js
+++ server/models/Users.js
@@ -4,6 +4,18 @@ const UserSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
+    },
+    id: {
+        type: Number,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
     }
 })
 
